26860e92a6ad51b01880fcf3e86e5284ad963422
GHY73 <41765394+GHY73@users.noreply.github.com>
2019-05-02 17:49:29 +0800

    Add files via upload
    标注主界面

---
+++ tool/basewin.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+# @Time :   19-5-2 下午5:07 
+# @Author :     HaiYang Gao
+# @OS：  ubuntu 16.04
+# @File :   basewin.py 
+
+import wx
+
+"""
+=============================step2:修改tag按钮的值=================================
+选择性更改从line43到line108的‘自定义按钮区’的按钮值
+1个修改的例子：
+原句：
+self.m_toggleBtn1 = wx.ToggleButton(self, wx.ID_ANY, u"1.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+要改的参数：u"1.自定义按钮"--->u"体育"
+"""
+
+
+class MyFrame1(wx.Frame):
+
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString, pos=wx.DefaultPosition,
+                          size=wx.Size(748, 748), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
+        self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
+        self.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_ACTIVECAPTION))
+
+        fgSizer3 = wx.FlexGridSizer(4, 1, 0, 0)
+        fgSizer3.SetFlexibleDirection(wx.BOTH)
+        fgSizer3.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+        main_text = wx.FlexGridSizer(2, 1, 0, 0)
+        main_text.SetFlexibleDirection(wx.BOTH)
+        main_text.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+        self.title = wx.StaticText(self, wx.ID_ANY, u"title", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTRE)
+        self.title.Wrap(-1)
+        self.title.SetMinSize(wx.Size(730, -1))
+
+        main_text.Add(self.title, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.content = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(730, 200),
+                                   wx.TE_MULTILINE | wx.TE_RICH | wx.TE_RICH2)
+        main_text.Add(self.content, 0, wx.ALL, 5)
+
+        fgSizer3.Add(main_text, 1, wx.ALIGN_CENTER, 5)
+
+        sbSizer12 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"tag类别"), wx.VERTICAL)
+
+        gSizer8 = wx.GridSizer(4, 5, 0, 0)
+
+        """
+        ========================自定义按钮区=========================================
+        自己修改tag按钮的名字，本次提供20个可选的tag。
+        其余代码不要变动有任何变动，包括顺序
+        """
+        self.m_toggleBtn1 = wx.ToggleButton(self, wx.ID_ANY, u"1.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+
+        gSizer8.Add(self.m_toggleBtn1, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn2 = wx.ToggleButton(self, wx.ID_ANY, u"2.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn2, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn3 = wx.ToggleButton(self, wx.ID_ANY, u"3.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn3, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn4 = wx.ToggleButton(self, wx.ID_ANY, u"4.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn4, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn5 = wx.ToggleButton(self, wx.ID_ANY, u"5.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn5, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn6 = wx.ToggleButton(self, wx.ID_ANY, u"6.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn6, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn7 = wx.ToggleButton(self, wx.ID_ANY, u"7.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn7, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn8 = wx.ToggleButton(self, wx.ID_ANY, u"8.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn8, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn9 = wx.ToggleButton(self, wx.ID_ANY, u"9.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn9, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn10 = wx.ToggleButton(self, wx.ID_ANY, u"10.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn10, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn11 = wx.ToggleButton(self, wx.ID_ANY, u"11.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn11, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn12 = wx.ToggleButton(self, wx.ID_ANY, u"12.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn12, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn13 = wx.ToggleButton(self, wx.ID_ANY, u"13.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn13, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn14 = wx.ToggleButton(self, wx.ID_ANY, u"14.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn14, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn15 = wx.ToggleButton(self, wx.ID_ANY, u"15.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn15, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn16 = wx.ToggleButton(self, wx.ID_ANY, u"16.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn16, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn17 = wx.ToggleButton(self, wx.ID_ANY, u"17.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn17, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn18 = wx.ToggleButton(self, wx.ID_ANY, u"18.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn18, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn19 = wx.ToggleButton(self, wx.ID_ANY, u"19.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn19, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_toggleBtn20 = wx.ToggleButton(self, wx.ID_ANY, u"20.自定义按钮", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer8.Add(self.m_toggleBtn20, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+        """
+        ========================自定义按钮区末尾=========================================
+        =======step2调整结束，step3在mainwin.py中，对write_a_record()函数进行更改==========
+        """
+
+        self.m_toggleBtn1.SetValue(True)
+
+        sbSizer12.Add(gSizer8, 1, wx.EXPAND, 5)
+
+        fgSizer3.Add(sbSizer12, 1, wx.EXPAND, 5)
+
+        sbSizer13 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"关键词抽取"), wx.VERTICAL)
+
+        gSizer12 = wx.GridSizer(6, 1, 0, 0)
+
+        self.address_radio = wx.RadioButton(self, wx.ID_ANY, u"地名: ", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer12.Add(self.address_radio, 0, wx.ALL, 5)
+
+        self.add_words = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                     0 | wx.NO_BORDER)
+        self.add_words.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_ACTIVECAPTION))
+        self.add_words.SetMinSize(wx.Size(710, -1))
+
+        gSizer12.Add(self.add_words, 0, wx.ALL, 5)
+
+        self.name_radio = wx.RadioButton(self, wx.ID_ANY, u"人员: ", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer12.Add(self.name_radio, 0, wx.ALL, 5)
+
+        self.name_words = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(710, 20),
+                                      0 | wx.NO_BORDER)
+        self.name_words.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_ACTIVECAPTION))
+
+        gSizer12.Add(self.name_words, 0, wx.ALL, 5)
+
+        self.keyword_radio = wx.RadioButton(self, wx.ID_ANY, u"中心词: ", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer12.Add(self.keyword_radio, 0, wx.ALL, 5)
+
+        self.key_words = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(710, -1),
+                                     0 | wx.NO_BORDER)
+        self.key_words.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_ACTIVECAPTION))
+
+        gSizer12.Add(self.key_words, 0, wx.ALL, 5)
+
+        sbSizer13.Add(gSizer12, 1, wx.EXPAND, 5)
+
+        fgSizer3.Add(sbSizer13, 1, wx.EXPAND, 5)
+
+        gSizer9 = wx.GridSizer(1, 2, 10, 0)
+
+        gSizer9.SetMinSize(wx.Size(-1, 50))
+        self.m_button40 = wx.Button(self, wx.ID_ANY, u"下一条", wx.DefaultPosition, wx.Size(200, -1), 0)
+        gSizer9.Add(self.m_button40, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.m_button41 = wx.Button(self, wx.ID_ANY, u"重载(暂时无用)", wx.DefaultPosition, wx.Size(200, -1), 0)
+        gSizer9.Add(self.m_button41, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        fgSizer3.Add(gSizer9, 1, wx.EXPAND, 5)
+
+        self.SetSizer(fgSizer3)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+        # Connect Events
+        self.content.Bind(wx.EVT_LEFT_UP, self.get_sellect_words)
+        self.m_toggleBtn1.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn2.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn3.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn4.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn5.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn7.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn9.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn10.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn11.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn13.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn14.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn15.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn17.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn19.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.m_toggleBtn20.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_label)
+        self.address_radio.Bind(wx.EVT_RADIOBUTTON, self.select_address)
+        self.name_radio.Bind(wx.EVT_RADIOBUTTON, self.select_name)
+        self.keyword_radio.Bind(wx.EVT_RADIOBUTTON, self.select_keyword)
+        self.m_button40.Bind(wx.EVT_BUTTON, self.next_doc)
+        self.m_button41.Bind(wx.EVT_BUTTON, self.reload)
+
+    def __del__(self):
+        pass
+
+    def get_sellect_words(self, event):
+        event.Skip()
+
+    def toggle_label(self, event):
+        event.Skip()
+
+    def select_address(self, event):
+        event.Skip()
+
+    def select_name(self, event):
+        event.Skip()
+
+    def select_keyword(self, event):
+        event.Skip()
+
+    def next_doc(self, event):
+        event.Skip()
+
+    def reload(self, event):
+        event.Skip()
---
+++ tool/mainwin.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+# @Time :   19-5-2 下午5:06 
+# @Author :     HaiYang Gao
+# @OS：  ubuntu 16.04
+# @File :   mainwin.py 
+
+from basewin import MyFrame1
+import wx
+import os
+import codecs
+import platform
+import json
+import sys
+import collections
+
+"""
+==============================step1：参数自定义=======================================
+自定义标注参数，自行调整;
+通常修改record_path，id_str，content_str就可以了
+"""
+Numbers = 99999  # 待标注的数据条数
+MaxLengthOfKeyWord = 15  # 中心词最大长度
+Tag_flag = 0  # value = 0 or -1; 如果Tag_flag=-1，则可以不选tag，也可以进行下一条内容的标注
+data_path = '../data/example.json'  # 待标注文件的路径
+record_path = '../data/records/record_01.txt'  # 标注文件的位置
+# 改为自己json文件对应的键值
+id_str = 'id'  # 主键，唯一
+content_str = 'content'  # 文本内容
+"""===============================step1调整结束，step2在mainwin.py中====================="""
+sys.setrecursionlimit(Numbers + 100)
+
+
+class MainWindows(MyFrame1):
+    record_file = record_path
+    data_file = data_path
+
+    def __init__(self, title):
+        MyFrame1.__init__(self, None)
+        self.cur_progress = 0  # 记录当前标注的条数
+        self.which_words_items = -1  # 记录哪种类型的关键词
+        self.textboxfont = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        self.content.SetFont(self.textboxfont)
+        self.read_data()
+        self.load_record()
+        self.reinit_record()
+        self.print_content()
+        self.keyword_radio.item_name = 'keyword'
+        self.name_radio.item_name = 'name'
+        self.address_radio.item_name = 'address'
+        self.add_words.item_name = 'address'
+        self.name_words.item_name = 'name'
+        self.key_words.item_name = 'keyword'
+
+    # 初始化
+    def reinit_record(self):
+        self.tags = []
+        self.address = []
+        self.person = []
+        self.keyword = []
+        self.which_words_items = -1
+        self.print_keywords()
+
+        for i in range(1, 21):
+            self.__getattribute__('m_toggleBtn' + str(i)).SetValue(False)
+
+        self.keyword_radio.SetValue(False)
+        self.name_radio.SetValue(False)
+        self.address_radio.SetValue(False)
+
+    def read_data(self):
+        with codecs.open(self.data_file, 'r', encoding='utf-8') as f:
+            data = json.load(f, object_pairs_hook=collections.OrderedDict)
+            self.data = []
+            for _, v in data['data'].items():
+                self.data.append(v)
+            self.data_size = len(self.data)
+
+    def print_content(self):
+        cur_doc = self.data[self.cur_progress]
+        if cur_doc[id_str] in self.ids:
+            self.cur_progress += 1
+            return self.print_content()
+        # 标注界面显示的标题
+        title = cur_doc[id_str] + '-----' + u'这里可以继续添加标题内容，当前显示了数据的id'
+        content = cur_doc[content_str]
+        self.title.SetLabel(title)
+        self.redefine_Textctrl_write(self.content, content)
+
+    # 定义按钮事件
+    def toggle_label(self, event):
+        eo = event.GetEventObject()
+        if eo.GetValue():
+            self.tags.append(eo.GetLabel())
+        else:
+            idx = self.tags.index(eo.GetLabel())
+            del (self.tags[idx])
+
+    # 加载标注记录, 获得续标断点
+    def load_record(self):
+        self.ids = []
+        if os.path.exists(self.record_file):
+            with codecs.open(self.record_file, 'r', encoding='utf-8') as f:
+                records = [d for d in f.read().split('\n') if len(d)]
+            for rd in records:
+                self.ids.append(json.loads(rd)[id_str])
+
+    # 显示选择的数据
+    def print_keywords(self):
+        if len(self.address):
+            self.redefine_Textctrl_write(self.add_words, '(' + '),('.join(self.address) + ')')
+        else:
+            self.redefine_Textctrl_write(self.add_words, '')
+        if len(self.person):
+            self.redefine_Textctrl_write(self.name_words, '(' + '),('.join(self.person) + ')')
+        else:
+            self.redefine_Textctrl_write(self.name_words, '')
+        if len(self.keyword):
+            self.redefine_Textctrl_write(self.key_words, '(' + '),('.join(self.keyword) + ')')
+        else:
+            self.redefine_Textctrl_write(self.key_words, '')
+
+    """
+    =============================step3:选择写入文件的内容=============================
+    """
+
+    # 写入记录
+    def write_a_record(self):
+        save_record = collections.OrderedDict()
+
+        save_record[id_str] = self.data[self.cur_progress][id_str]
+        save_record[content_str] = self.data[self.cur_progress][content_str]
+        save_record[u'地名'] = self.address
+        save_record[u'人员'] = self.person
+        save_record[u'中心词'] = self.keyword
+        save_record['tags'] = self.tags
+        """
+        例子：添加一个写入。
+        save_record[u'作者'] = self.data[self.cur_progress]['author']  
+        save_record['time'] = self.data[self.cur_progress]['time']
+        # 这里的'author'必须是json文件中的键，中文前要加'u'
+
+        不写入：
+        不想写入，就注释或删掉某一句，如 # line126，则不会写入地名信息
+        """
+        save_text = json.dumps(save_record, ensure_ascii=False)
+        with codecs.open(self.record_file, 'a', encoding='utf-8') as f:
+            f.write(save_text + '\n')
+
+    """
+    ========step3调整结束，可运行"mainwin.py"，开始标注================================
+    """
+
+    def next_doc(self, event):
+        if len(self.tags) == Tag_flag:
+            wx.MessageBox(u'您没有选择任何标签', u'警告', wx.OK | wx.ICON_EXCLAMATION)
+        else:
+            self.write_a_record()
+            self.cur_progress += 1
+            if self.cur_progress == self.data_size:
+                self.exit(0)
+            self.reinit_record()
+            self.print_content()
+            self.print_keywords()
+
+    def get_sellect_words(self, event):
+        words = self.content.GetStringSelection()
+        if self.which_words_items == -1 and len(words) > 0:
+            wx.MessageBox(u'请选择一种要标注的关键字类型', u'警告', wx.OK | wx.ICON_EXCLAMATION)
+            return False
+        elif len(words) > MaxLengthOfKeyWord:
+            a = wx.MessageBox(words + ' \n\n是否选为关键词?', '关键词过长验证', wx.YES_NO | wx.ICON_EXCLAMATION)
+            if not a == wx.YES:
+                return False
+        elif len(words) < 2:
+            return False
+
+        if self.which_words_items == 1:
+            self.address.append(words)
+        elif self.which_words_items == 2:
+            self.person.append(words)
+        elif self.which_words_items == 3:
+            self.keyword.append(words)
+        self.print_keywords()
+
+    def select_address(self, event):
+        self.which_words_items = 1
+
+    def select_name(self, event):
+        self.which_words_items = 2
+
+    def select_keyword(self, event):
+        self.which_words_items = 3
+
+    def del_words(self, event):
+        print(dir(event))
+
+    def exit(self, flag):
+        if flag == 0:
+            wx.MessageBox(u'当前标注已经完成, 感谢您的参与', u'警告', wx.OK | wx.ICON_EXCLAMATION)
+
+    # 根据不同平台使用不同的Textctrl方法
+    def redefine_Textctrl_write(self, object, string):
+        if 'windows' in platform.system().lower():
+            object.SetLabel(string)
+        else:
+            object.SetValue(string)
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    main_win = MainWindows(title='文本标注工具')
+    main_win.Show()
+    app.MainLoop()
