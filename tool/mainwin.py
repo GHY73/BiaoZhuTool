7d59702b9b94a4689c9062b2db4986a6c1602618
GHY73 <41765394+GHY73@users.noreply.github.com>
2019-05-21 15:23:14 +0800

    Update mainwin.py

--- tool/mainwin.py
+++ tool/mainwin.py
@@ -24,6 +24,7 @@ Tag_flag = 0  # value = 0 or -1; 如果Tag_flag=-1，则可以不选tag，也可
 data_path = '../data/example.json'  # 待标注文件的路径
 record_path = '../data/records/record_01.txt'  # 标注文件的位置
 # 改为自己json文件对应的键值
+# 中文键要在字符串前加上u，比如id_str = u'序号'
 id_str = 'id'  # 主键，唯一
 content_str = 'content'  # 文本内容
 """===============================step1调整结束，step2在basewin.py中====================="""
